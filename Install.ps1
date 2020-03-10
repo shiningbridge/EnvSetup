45f0e3bcbc8fb7d26a43bf6ddd3b4910c6680cac
Edi Wang <Edi.Wang@outlook.com>
2020-03-10 14:13:18 +0800

    Remove Microsoft.GetHelp

--- Install.ps1
+++ Install.ps1
@@ -48,7 +48,8 @@ $uwpRubbishApps = @(
     "Microsoft.YourPhone",
     "Microsoft.MicrosoftOfficeHub",
     "Fitbit.FitbitCoach",
-    "4DF9E0F8.Netflix")
+    "4DF9E0F8.Netflix",
+    "Microsoft.GetHelp")
 
 foreach ($uwp in $uwpRubbishApps) {
     Get-AppxPackage -Name $uwp | Remove-AppxPackage
